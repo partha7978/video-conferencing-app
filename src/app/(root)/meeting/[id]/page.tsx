71894be8f009b4d50425dccd914677538c542463
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2024-05-12 22:58:06 +0530

    added Meeting call setup status check using StreamCall

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+import { useGetCallById } from "../../../../../hooks/useGetCallById";
+import { Loader } from "lucide-react";
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+  const { call, isCallLoading } = useGetCallById(id);
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if( !isLoaded || isCallLoading) return <Loader />
   return (
-    <div>Meeting Room: #{params.id}</div>
-  )
-}
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
 
-export default Meeting
+export default Meeting;
